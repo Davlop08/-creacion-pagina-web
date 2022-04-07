fd080092fde219f2967e6dbef32873dad2a06acd
Davlop08 <jdlqdavlop@gmail.com>
2022-04-06 23:21:01 -0300

    se realizaron mejoras de estilos, se agregaron imagenes en las tarjetas y un separador inferior

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,10 +42,11 @@ export default function Home() {
 
         <section className={styles.experiences}>
           <div className={styles.experiences_body}>
-            <h2 className={styles.experiences_title}>Modificar titulo 1</h2>
+            <h2 className={styles.experiences_title}>¿Qué te ofrecemos?</h2>
             <div className={styles.experiences_grid}>
 
               <div className={styles.experience + ' ' + styles.experience_1}>
+                <h4 className={styles.experience_title_1}>Pagina web simple</h4>
                 <div className={styles.experience_description}>
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla repudiandae debitis ipsa eligendi autem tempora recusandae inventore suscipit molestias tenetur.
                 </div>
@@ -54,13 +55,14 @@ export default function Home() {
               <Link href="/">
                 <a className={styles.pages_radius}>
                   <div className={styles.empty + ' ' + styles.empty_1}>
-                    <Image src="/imagenes/238.jpg" height={300} width={300} alt="imagen"/>
+                    <Image src="/imagenes/single_page.png" height={250} width={250} alt="imagen"/>
                   </div>
                 </a>
               </Link>
 
 
               <div className={styles.experience + ' ' + styles.experience_2}>
+                <h4 className={styles.experience_title_2}>Sitio web básico</h4>
                 <div className={styles.experience_description}>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat laborum asperiores doloremque ad placeat ex odit quod eos, autem vitae ut ipsam aliquid ab quae.
                 </div>
@@ -69,12 +71,13 @@ export default function Home() {
               <Link href="/">
                   <a className={styles.pages_radius}>
                     <div className={styles.empty + ' ' + styles.empty_2}>
-                      <Image src="/imagenes/238.jpg" height={300} width={300} alt="imagen"/>
+                      <Image src="/imagenes/basic_site.png" height={250} width={250} alt="imagen"/>
                     </div>
                   </a>
               </Link>
 
               <div className={styles.experience + ' ' + styles.experience_3}>
+                <h4 className={styles.experience_title_3}>Tienda en línea</h4>
                 <div className={styles.experience_description}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum rerum perferendis odit, et illo architecto.
                 </div>
@@ -83,13 +86,14 @@ export default function Home() {
               <Link href="/">
                   <a className={styles.pages_radius}>
                     <div className={styles.empty + ' ' + styles.empty_3}>
-                      <Image src="/imagenes/238.jpg" height={300} width={300} alt="imagen"/>
+                      <Image src="/imagenes/comerce_site.png" height={250} width={250} alt="imagen"/>
                     </div>
                   </a>
               </Link>
             
 
               <div className={styles.experience + ' ' + styles.experience_4}>
+                <h4 className={styles.experience_title_4}>Sitio autoadministrable</h4>
                 <div className={styles.experience_description}>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda tenetur aliquam esse incidunt. Eaque reiciendis vitae doloremque tenetur et! Eveniet unde ex soluta accusantium aut dolore culpa eius optio assumenda.
                 </div>
@@ -98,7 +102,7 @@ export default function Home() {
               <Link href="/">
                   <a className={styles.pages_radius}>
                     <div className={styles.empty + ' ' + styles.empty_4}>
-                      <Image src="/imagenes/238.jpg" height={300} width={300} alt="imagen"/>
+                      <Image src="/imagenes/self_admin.png" height={250} width={250} alt="imagen"/>
                     </div>
                   </a>
               </Link>
@@ -108,6 +112,7 @@ export default function Home() {
           </div>
         </section>
 
+      <div className={styles.separator_bottom}></div>
       </Layout>
     </Fragment>
 

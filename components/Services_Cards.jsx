aea4f9d91ab21e47ee02ea5e980b83f1e6c2ca36
Davlop08 <jdlqdavlop@gmail.com>
2022-04-17 18:20:34 -0300

    se cambio las imagenes de las tarjetas por svgs y se borraron las que estaban

--- components/Services_Cards.jsx
+++ components/Services_Cards.jsx
@@ -32,7 +32,7 @@ export default function Services_Cards() {
                     <Link href={`${globals.direction}/paginas`}>
                     <a className={styles.pages_radius}>
                         <div className={styles.empty + ' ' + styles.empty_1}>
-                        <Image src="/imagenes/single_page.png" height={250} width={250} alt="imagen"/>
+                        <Image src="/imagenes/paginas.svg" height={250} width={250} alt="imagen"/>
                         <div className={styles.card_info}>
                             <h5>Desde $9.900</h5>
                             <p>Ver más</p>                  
@@ -57,7 +57,7 @@ export default function Services_Cards() {
                     <Link href={`${globals.direction}/publicidad`}>
                         <a className={styles.pages_radius}>
                         <div className={styles.empty + ' ' + styles.empty_2}>
-                            <Image src="/imagenes/basic_site.png" height={250} width={250} alt="imagen"/>
+                            <Image src="/imagenes/google-icon.svg" height={250} width={250} alt="imagen"/>
                             <div className={styles.card_info}>
                             <h5>Desde $4.900 + inversión
                             </h5>
@@ -81,7 +81,7 @@ export default function Services_Cards() {
                     <Link href={`${globals.direction}/mantenimiento`}>
                         <a className={styles.pages_radius}>
                         <div className={styles.empty + ' ' + styles.empty_3}>
-                            <Image src="/imagenes/comerce_site.png" height={250} width={250} alt="imagen"/>
+                            <Image src="/imagenes/mantenimiento_web_2.svg" height={250} width={250} alt="imagen"/>
                             <div className={styles.card_info}>
                             <h5>Desde $999</h5>
                             <p>Ver más</p>                  
@@ -103,7 +103,7 @@ export default function Services_Cards() {
                     <Link href={`${globals.direction}/paquetes`}>
                         <a className={styles.pages_radius}>
                         <div className={styles.empty + ' ' + styles.empty_4}>
-                            <Image src="/imagenes/self_admin.png" height={250} width={250} alt="imagen"/>
+                            <Image src="/imagenes/paquetes.svg" height={250} width={250} alt="imagen"/>
                             <div className={styles.card_info}>
                             <h5>Desde $23.900</h5>
                             <p>Ver más</p>                  

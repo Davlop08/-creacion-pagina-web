3bba8d92ee18a11f8f1a43ef12e0db3921f407a3
Davlop08 <jdlqdavlop@gmail.com>
2022-04-19 22:34:15 -0300

    se agregó descripción de caracteristicas y el formulario a la pagina landing, y se modifico algunas imagenes de las tarjetas

--- components/Pages_Includes.jsx
+++ components/Pages_Includes.jsx
@@ -4,68 +4,78 @@ import styles from "../styles/Includes.module.css"
 export default function Pages_Includes({price, pages, imgs, cart, stock, mail, mp}) {
     return (
         <section className={styles.site_info_container}>
-        <div className={styles.site_info_content_container}>
-            <div className={`${styles.card_item} ${styles.card_item_1}`}>
-                <h5>Precio</h5>
-                <p>{price}</p>
+            
+            <div className={styles.site_info_description_container}>
+                <div className={styles.site_info_description_content_container}>
+                    <h4 className={styles.site_info_description_title}>
+                        Tu sitio contiene las siguientes características, los cuadros marcados como 
+                        &ldquo;No aplica&ldquo; no están incluidos:
+                    </h4>
+                </div>
             </div>
-            <div className={`${styles.card_item} ${styles.card_item_2}`}>
-                <h5>Páginas</h5>
-                <p>{pages}</p>
+
+            <div className={styles.site_info_content_container}>
+                <div className={`${styles.card_item} ${styles.card_item_1}`}>
+                    <h5>Precio</h5>
+                    <p>{price}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_2}`}>
+                    <h5>Páginas</h5>
+                    <p>{pages}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_3}`}>
+                    <h5>Imágenes</h5>
+                    <p>{imgs}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_4}`}>
+                    <h5>Botón de WhatsApp</h5>
+                    <p>Si</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_13}`}>
+                    <h5>Formulario</h5>
+                    <p>Para que te contacten directo desde la web</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_6}`}>
+                    <h5>Redes sociales</h5>
+                    <p>Enlaces a tus redes sociales</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_14}`}>
+                    <h5>Correo profesional</h5>
+                    <p>{mail}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_5}`}>
+                    <h5>Hosting</h5>
+                    <p>Alojamiento gratuito por 1 año</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_7}`}>
+                    <h5>Dominio</h5>
+                    <p>Dominio.com gratuito por 1 año</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_10}`}>
+                    <h5>SSL</h5>
+                    <p>Tu sitio será seguro</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_8}`}>
+                    <h5>Perfil de negocio</h5>
+                    <p>Tu negocio aparecerá en los resultados por zonas en Google y Maps</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_9}`}>
+                    <h5>Google Analytics</h5>
+                    <p>Podrás ver las estadísticas de tu sitio</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_11}`}>
+                    <h5>Carrito de compras</h5>
+                    <p>{cart}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_12}`}>
+                    <h5>Control de stock</h5>
+                    <p>{stock}</p>
+                </div>
+                <div className={`${styles.card_item} ${styles.card_item_15}`}>
+                    <h5>MercadoPago</h5>
+                    <p>{mp}</p>
+                </div>
             </div>
-            <div className={`${styles.card_item} ${styles.card_item_3}`}>
-                <h5>Imágenes</h5>
-                <p>{imgs}</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_4}`}>
-                <h5>Botón de WhatsApp</h5>
-                <p>Si</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_13}`}>
-                <h5>Formulario</h5>
-                <p>Para que te contacten directo desde la web</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_6}`}>
-                <h5>Redes sociales</h5>
-                <p>Enlaces a tus redes sociales</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_14}`}>
-                <h5>Correo profesional</h5>
-                <p>{mail}</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_5}`}>
-                <h5>Hosting</h5>
-                <p>Alojamiento gratuito por 1 año</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_7}`}>
-                <h5>Dominio</h5>
-                <p>Dominio.com gratuito por 1 año</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_10}`}>
-                <h5>SSL</h5>
-                <p>Tu sitio será seguro</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_8}`}>
-                <h5>Perfil de negocio</h5>
-                <p>Tu negocio aparecerá en los resultados por zonas en Google y Maps</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_9}`}>
-                <h5>Google Analytics</h5>
-                <p>Podrás ver las estadísticas de tu sitio</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_11}`}>
-                <h5>Carrito de compras</h5>
-                <p>{cart}</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_12}`}>
-                <h5>Control de stock</h5>
-                <p>{stock}</p>
-            </div>
-            <div className={`${styles.card_item} ${styles.card_item_15}`}>
-                <h5>MercadoPago</h5>
-                <p>{mp}</p>
-            </div>
-        </div>
-    </section>
+        </section>
     )
 }
--- pages/paginas/landing.jsx
+++ pages/paginas/landing.jsx
@@ -5,6 +5,7 @@ import Layout from "../../components/Layout"
 import HeadMeta from "../../components/HeadMeta"
 import styles from "../../styles/Individual_Pages.module.css"
 import Pages_Includes from "../../components/Pages_Includes"
+import ContactForm from "../../components/ContactForm"
 
 
 export default function landing() {
@@ -19,11 +20,12 @@ export default function landing() {
                     <div className={styles.entry_content_container}>
                         <h1 className={styles.entry_content_title}>Landing Page</h1>
                         <p className={styles.entry_content_description}>
-                            Landing Page o página de aterrizaje, es aquella en donde mostramos la información más 
-                            relevante de nuestro producto o servicio, y consiste en una sola página en donde mostramos 
-                            información básica y concreta de lo que hacemos, algunas imágenes, información de contacto 
+                            Landing Page o página de aterrizaje, es aquella en donde mostrás la información más 
+                            relevante de tu producto o servicio, y consiste en una sola página en donde mostrás 
+                            información básica y concreta de lo que haces, algunas imágenes, información de contacto 
                             y/o un formulario, con el propósito de captar la atención de potenciales clientes. 
-                            La principal función de la Landing Page es simple, guiar al cliente sutilmente para haga lo que queremos, 
+                            La principal función de la Landing Page es simple, convertir visitantes en clientes, 
+                            y esto se logra guiando a dicho visitante sutilmente para haga lo que queremos, 
                             que en este caso es que se contacte con vos, o que te conozca y adquiera lo que ofreces por medio
                             de un estratégico diseño que te ofrecemos. No dudes más y empezá a potenciar tu negocio!
                         </p>
@@ -31,6 +33,20 @@ export default function landing() {
                 </section>
 
                 <Pages_Includes price="AR$9.900" pages="1" imgs="10" cart="No aplica" stock="No aplica" mail="No aplica" mp="No aplica"/>
+
+                <section className={styles.entry_container}>
+                    <div className={styles.entry_content_container}>
+                        <p className={styles.entry_content_description}>Para contratar este servicio escribinos por 
+                            <Link  
+                            href="https://api.whatsapp.com/send?phone=5491162039196&text=Hola%2C%20estoy%20interesado%20en%20la%20p%C3%A1gina%20Landing">
+                                <a className={styles.contact_wa_link}> WhatsApp </a>
+                            </Link>
+                            o dejanos tus datos y nos comunicaremos con vos.
+                        </p>
+                    </div>
+                </section>
+
+                <ContactForm/>
             </Layout>
         </Fragment>
     )
